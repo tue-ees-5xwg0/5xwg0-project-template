2d8eb7514968b5b42722182cf4eb36f0263d9caa
Tony Xiang <tony.xiang@alliander.com>
2024-03-26 10:30:22 +0100

    pylint
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- src/power_system_simulation/simple_function.py
+++ src/power_system_simulation/simple_function.py
@@ -1,2 +1,15 @@
+"""
+A module with simple function
+"""
+
 def add(a: int, b: int) -> int:
+    """ Add two numbers
+
+    Args:
+        a: number a
+        b: number b
+
+    Returns:
+        added number
+    """
     return a + b
